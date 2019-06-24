34c452005a87fb9a7eafa6799113797955f56244
Somsubhra Das <das.somsubhra1@gmail.com>
2019-06-24 22:30:01 +0530

    Updates

---
+++ precache-manifest.7cb457fd84212a305de73e6e8d036526.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b5a8324d4c1e2fc4390dc1b895ce20e",
+    "url": "/React-Pagination/index.html"
+  },
+  {
+    "revision": "a7677ba7f01b83c07e1a",
+    "url": "/React-Pagination/static/css/main.f9cbafd3.chunk.css"
+  },
+  {
+    "revision": "79bd3f250d67764ffd95",
+    "url": "/React-Pagination/static/js/2.e163c10d.chunk.js"
+  },
+  {
+    "revision": "a7677ba7f01b83c07e1a",
+    "url": "/React-Pagination/static/js/main.8e2d9cb4.chunk.js"
+  },
+  {
+    "revision": "7ffb1037faea8142ed6c",
+    "url": "/React-Pagination/static/js/runtime~main.b5746b85.js"
+  }
+]);
